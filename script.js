99a06875092c27558152c8737eacacf4f0cd10c1
teja66-hash <122727734+teja66-hash@users.noreply.github.com>
2023-03-07 22:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const taskInput = document.getElementById("taskInput");
-const addButton = document.getElementById("addButton");
-const taskList = document.getElementById("taskList");
-
-addButton.addEventListener("click", addTask);
-
 function addTask() {
-  if (taskInput.value !== "") {
-    const task = document.createElement("li");
-    task.innerHTML = taskInput.value;
-    taskList.appendChild(task);
-    taskInput.value = "";
-    task.addEventListener("click", () => {
-      task.classList.toggle("completed");
-    });
+  var task = document.getElementById("task").value;
+  if (task === "") {
+    alert("Please enter a task.");
+    return;
   }
+  var list = document.getElementById("taskList");
+  var li = document.createElement("li");
+  var text = document.createTextNode(task);
+  li.appendChild(text);
+  var button = document.createElement("button");
+  button.appendChild(document.createTextNode("Delete"));
+  button.addEventListener("click", function () {
+    list.removeChild(li);
+  });
+  li.appendChild(button);
+  list.appendChild(li);
+  document.getElementById("task").value = "";
+}
+
+function deleteTask(button) {
+  var li = button.parentNode;
+  var list = li.parentNode;
+  list.removeChild(li);
 }
